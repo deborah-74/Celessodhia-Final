1177a653976fbc1a31dbc5762b9046fb202c773c
deborah-74 <74653634+deborah-74@users.noreply.github.com>
2021-09-22 12:07:37 +0400

    Final Celessodhia Project

---
+++ Assets/ArtisticMechanics/SKY.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SKY: MonoBehaviour
+{
+
+      public float RotateSpeed = 1.2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+    }
+}
